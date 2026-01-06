edd92cfbe248813902d1382e4b008e4ad6a33f47
BhanuprakashCH10 <cs23b012@iittp.ac.in>
2026-01-06 15:20:53 +0530

    Created a NRZ code varibles

---
+++ cs23b012_nrz_am.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = [1, 0, 1, 0, 1, 1, 0]
+samples_per_bit = 200
+fc = 20  
+fs = samples_per_bit  # Sampling frequency
+Ac = 1  # Carrier amplitude
+Am = 1  # Message amplitude
+
