15639398a8ef0da5b3dafb1380c6b585cf21189a
BhanuprakashCH <cs23b012@iittp.ac.in>
2026-01-06 15:24:16 +0530

    created sinusoidal carrier signal with carrier frequency as fc=20 Hz

--- cs23b012_nrz_am.py
+++ cs23b012_nrz_am.py
@@ -8,3 +8,10 @@ fs = samples_per_bit  # Sampling frequency
 Ac = 1  # Carrier amplitude
 Am = 1  # Message amplitude
 
+nrz_signal = np.repeat([1 if bit == 1 else -1 for bit in data],
+                       samples_per_bit)
+
+t = np.arange(len(nrz_signal)) / fs
+
+
+carrier = Ac * np.sin(2 * np.pi * fc * t)
